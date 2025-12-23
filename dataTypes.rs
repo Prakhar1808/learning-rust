b1e44591511a2a01d30ba481a672b983a315cfa9
Prakhar1808 <prakharsharma.home@gmail.com>
2025-12-23 21:29:17 +0530

    Create dataTypes.rs

---
+++ dataTypes.rs
@@ -0,0 +1,19 @@
+fn main(){
+    //scalar types (Like C Primitives)
+    let int: i32 = -5           //Signed 32-bit (also i8, i16, i64, i128)
+    let uint u32 = 5;           //Unsigned
+    let float: f64 = 3.14;      //Also f32
+    let boolean: bool = true;
+    let character: char = 'z';  //unicode, 4 bytes (unlike C's char)
+
+    // Compound types
+    let tuple: (i32, f64, char) = (500, 6.4, 'J');
+    let (x,y,z) = tuple:        //destructing (like Python)
+    
+    let array: [i32; 5] = [1, 2, 3, 4, 5]; //fixed size stack allocated
+    let slice: &[i32] = &array[1..3]        //Reference to portion
+
+    //String Types
+    let string_literal: &str = "hello"; //immutable reference, stack
+    let string_object: String = String::from("hello"); //Heap Allocated, growable
+}
