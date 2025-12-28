2e2b16883bded4345e933d216bc09c7616629b88
Prakhar1808 <prakharsharma.home@gmail.com>
2025-12-28 21:05:32 +0530

    different size integers (and Bitwise Operators in future) in rust

---
+++ diffIntSizes.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // Different Interger Sizes
+    let small: i8 = 127; //Max Value for i8
+    let medium: i16 = 3200;
+    let normal: i32 = 2000000000; // 10 digit integer
+    let big: i64 = 9000000000000000000; //19 digit integer
+    
+
+    // there's also overflow (Checked in debug mode)
+    // let overflow = small + 1; // this would panic in debug mode
+
+
+    // Bitwise Operations
+    let x: u8 = 0b1010_1010; // Binary literal
+    let y: u8 = 0b1100_1100;
+
+
+    //TBC
