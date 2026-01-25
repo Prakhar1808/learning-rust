dd275121901a24467609d4570f2f10017276450b
Prakhar1808 <prakharsharma.home@gmail.com>
2026-01-25 22:15:36 +0530

    small revision
    reintroduction to rust variables and print statements

--- hello-world.rs
+++ hello-world.rs
@@ -1,4 +1,5 @@
 //main.rs
 fn main() {
-    println!("Hello, Rust World");
+    println!("Hello, Rust World"); //print!() or println!() both work
+    //but println!() adds new line at the end of the output
 }
--- variables.rs
+++ variables.rs
@@ -2,12 +2,15 @@ fn main() {
     //immutable by default
     let x=5;
     // x=6; // ERROR: cannot assign twice to immutable variable
-    
+    assert_eq!(x, 5);
+    println!("Success!");
+
 
     //mutable variables need explicit 'mut'
     let mut y = 10;
-    y=15; //no error
-
+    y+=15; //no error //25
+    assert_eq!(y, 25);
+    println!("Success!");
 
     //Type annotations (optional, like c but with inference)
     let z: i32 = 20;
@@ -16,5 +19,17 @@ fn main() {
     let a=5;
     let a=a+1; // New Variable, can change type
     let a="I have evolved twin, this is my final form, i am a string"; // works with shadowing
+    //more about shadowing
+    let i: i32 = 5;
+    {
+        let i= 12;
+        assert_eq!(i, 12);
+    }
 
+    assert_eq!(i, 5);
+    let i = 40;
+    assert_eq!(i, 40);
+    println!("{}", x); //40
+
+    let w: i32; //this will give a warning, because uninitialized but un used
 }
