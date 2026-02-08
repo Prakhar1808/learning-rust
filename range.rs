573b2ba9a1dc1993c81dc4b48b6da63526754bcf
Prakhar1808 <prakharsharma.home@gmail.com>
2026-02-08 22:11:53 +0530

    chore(range): add function for range
    example function for range in rust programming

---
+++ range.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut sum: i32 = 0; //i32 default
+    for i in -3..3 { //range -3,-2,-1.....1,2 #end point is excluded
+        sum += 1
+    }
+
+    assert!(Sum == -3);
+
+    for c in 'a'.. = 'z' { // a,b,c...,z
+        println!("{}",c as u8);
+    }
+}
