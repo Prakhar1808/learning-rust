1a52bf1294c8235c01e4c38938b729b18392d534
Prakhar1808 <Prakhar1808@users.noreply.github.com>
2026-05-29 19:11:50 +0530

    guessing_game: add random function and use the rand crate

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,11 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    println!("The Secret number is: {secret_number}");
     println!("Please input your Guess");
 
     let mut guess = String::new();
