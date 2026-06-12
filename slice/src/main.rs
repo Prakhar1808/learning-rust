ad8d914bb0ba60a6466aa8d57eba4900f76eeaf2
Prakhar1808 <Prakhar1808@users.noreply.github.com>
2026-06-13 00:52:24 +0530

    slice: &String -> &str

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -2,7 +2,9 @@ fn main() {
     println!("String Slice");
 }
 
-fn first_word(s: &String) -> &str {
+// fn first_word(s: &String) -> &str { // old line
+fn first_word(s: &str) -> &str { // new one allows us to use the fun on `&String` and `&str` both
+                                 // now compatible with both string variables and string literals
     let bytes = s.as_bytes();
 
     for(i, &item) in bytes.iter().enumerate() {
